ac0fdbda265c4e0060be3e7aeb26c71a0292325b
allzhou <allzhou@stubhub.com>
2018-04-08 17:29:24 +0800

    update sort

--- src/com/zy/sort/BubbleSort.java
+++ src/com/zy/sort/BubbleSort.java
@@ -1,5 +1,7 @@
 package com.zy.sort;
 
+import javax.security.auth.kerberos.KerberosKey;
+
 /**
  * 冒泡排序：稳定 若文件初状为正序，则一趟起泡就可完成排序，排序码的比较次数为n-1，且没有记录移动，时间复杂度是O(n)
  * 若文件初态为逆序，则需要n-1趟起泡，每趟进行n-i次排序码的比较，比较和移动次数均达到最大值∶O(n2)
@@ -11,25 +13,47 @@ package com.zy.sort;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		int[] a = { 49, 38, 65, 97, 176, 213, 227, 49, 78, 34, 12, 164, 11, 18, 1 };
+		int[] a = { 49, 38, 65, 97, 176, 113, 127, 49, 78, 34, 12, 164, 11, 18, 1 , 99, 44, 100, 101, 55, 102, 23, 58, 69, 54, 12, 2, 6, 200, 201, 
+				202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214};
 		System.out.println("排序前: ");
 		print(a);
-		sort(a);
+		long startTime = System.nanoTime();
+		//bubbleSort1(a);
+		//bubbleSort2(a);
+		bubbleSort3(a);
+		long endTime = System.nanoTime();
+		System.out.println("排序运行时间：" + (endTime - startTime) + "ns");    //输出程序运行时间
 		System.out.println("排序后: ");
 		print(a);
 	}
 
-	private static void sort(int[] data) {
+	//原始思想
+	public static void bubbleSort1(int[] data){
+		int n = data.length;
+		for(int i = 0; i < n; i++){
+			for(int j = 1; j < n - i; j++){
+				if(data[j - 1] > data[j]){
+					int temp = data[j - 1];
+					data[j - 1] = data[j];
+					data[j] = temp;
+				}
+			}
+		}
+	}
+	
+	//第一次优化，加入flag标志
+	private static void bubbleSort2(int[] data) {
 		boolean needNextPass = true;
-		for (int i = 1; i < data.length && needNextPass; i++) {
+		int n = data.length;
+		for (int i = 0; i < n && needNextPass; i++) {
 			// Array may be sorted and next pass not needed.
 			needNextPass = false;
 			// 这里-i主要是每遍历一次都把最大的i个数沉到最底下去了，没有必要再替换了
-			for (int j = 0; j < data.length - i; j++) {
-				if (data[j] > data[j + 1]) {
+			for (int j = 1; j < n - i; j++) {
+				if (data[j - 1] > data[j]) {
 					int temp = data[j];
-					data[j] = data[j + 1];
-					data[j + 1] = temp;
+					data[j] = data[j - 1];
+					data[j - 1] = temp;
 					needNextPass = true;
 				}
 			}
@@ -37,6 +61,25 @@ public class BubbleSort {
 
 	}
 
+	//优化后的，对于后边已经排好序的，只需要进行一次比较
+	 private static void bubbleSort3(int[] data){
+		 int i, j;
+		 int flag = data.length;//flag来记录最后交换的位置，也就是排序的尾边界
+		 while(flag > 0){//排序未结束标志
+			 j = flag;//j 来记录遍历的尾边界
+			 flag = 0;
+			 for(i = 1; i < j; i++){
+				 if(data[i - 1] > data[i]){//前面的数字大于后面的数字就交换
+					 int temp = data[i - 1];
+					 data[i - 1] = data[i];
+					 data[i] = temp;
+					//表示交换过数据;
+					 flag = i;//记录最新的尾边界.
+				 }
+			 }
+		 }
+	 }
+	
 	private static void print(int[] data) {
 		for (int i = 0; i < data.length; i++) {
 			System.out.print(data[i] + " ");
--- src/com/zy/sort/quickSort.java
+++ src/com/zy/sort/quickSort.java
@@ -1,52 +1,89 @@
 package com.zy.sort;
+
 /**
  * 1、基本思想：选择一个基准元素,通常选择第一个元素或者最后一个元素,通过一趟扫描，将待排序列分成两部分,
- * 一部分比基准元素小,一部分大于等于基准元素,此时基准元素在其排好序后的正确位置,然后再用同样的方法递归地
- * 排序划分的两部分。
- * 快速排序是不稳定的排序。
-　　快速排序的时间复杂度为O(nlogn)。
-　　当n较大时使用快排比较好，当序列基本有序时用快排反而不好。
+ * 一部分比基准元素小,一部分大于等于基准元素,此时基准元素在其排好序后的正确位置,然后再用同样的方法递归地 排序划分的两部分。 快速排序是不稳定的排序。
+ * 快速排序的时间复杂度为O(nlogn)。 当n较大时使用快排比较好，当序列基本有序时用快排反而不好。
+ * 
  * @author zy
  *
  */
 public class quickSort {
 
 	public static void main(String[] args) {
-		int[] a = {49,38,65,97,176,213,227,49,78,34,12,164,11,18,1};
+		int[] a = { 49, 38, 65, 97, 176, 213, 227, 49, 78, 34, 12, 164, 11, 18, 1 };
 		System.out.println("排序前: ");
 		print(a);
-		sort(a,0,a.length-1);
+		//sort(a, 0, a.length - 1);
+		quickSort(a, 0, a.length - 1);
 		System.out.println("排序后: ");
 		print(a);
 	}
-	
+
 	private static void sort(int[] data, int low, int high) {
-		if(low < high){//如果不加这个判断递归会无法退出导致堆栈溢出异常
-			int middle = getMiddle(data,low,high);
+		if (low < high) {// 如果不加这个判断递归会无法退出导致堆栈溢出异常
+			int middle = getMiddle(data, low, high);
 			sort(data, 0, middle - 1);
 			sort(data, middle + 1, high);
 		}
 	}
 
 	private static int getMiddle(int[] data, int low, int high) {
-		int temp = data[low];//基准元素
-		while(low < high){
-			//找到比基准元素小的元素位置
-			while(low < high && data[high] >= temp){
+		int temp = data[low];// 基准元素
+		while (low < high) {
+			// 找到比基准元素小的元素位置
+			while (low < high && data[high] >= temp) {
 				high--;
 			}
 			data[low] = data[high];
-			while(low < high && data[low] <= temp){
+			while (low < high && data[low] <= temp) {
 				low++;
 			}
 			data[high] = data[low];
 		}
-		data[low] = temp;
+		data[high] = temp;
 		return low;
 	}
 
+	private static void quickSort(int[] a, int low, int high) {
+		// 1,找到递归算法的出口
+		if (low > high) {
+			return;
+		}
+		// 2, 存
+		int i = low;
+		int j = high;
+		// 3,key
+		int key = a[low];
+		// 4，完成一趟排序
+		while (i < j) {
+			// 4.1 ，从右往左找到第一个小于key的数
+			while (i < j && a[j] > key) {
+				j--;
+			}
+			// 4.2 从左往右找到第一个大于key的数
+			while (i < j && a[i] <= key) {
+				i++;
+			}
+			// 4.3 交换
+			if (i < j) {
+				int p = a[i];
+				a[i] = a[j];
+				a[j] = p;
+			}
+		}
+		// 4.4，调整key的位置
+		int p = a[i];
+		a[i] = a[low];
+		a[low] = p;
+		// 5, 对key左边的数快排
+		quickSort(a, low, i - 1);
+		// 6, 对key右边的数快排
+		quickSort(a, i + 1, high);
+	}
+
 	private static void print(int[] data) {
-		for(int i = 0; i < data.length; i++){
+		for (int i = 0; i < data.length; i++) {
 			System.out.print(data[i] + " ");
 		}
 		System.out.println();
